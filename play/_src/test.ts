b50e5fba287ec790fac351ce4cfc85c72009498b
Timm Stelzer <timmstelzer@gmail.com>
2023-02-05 21:56:42 +0100

    reproduce: jump-to-definition tsplus problem

--- play/_src/test.ts
+++ play/_src/test.ts
@@ -1,6 +1,10 @@
 // import "./_global.js"
+import * as Foo from "./foo.js"
 
 console.log("hello3 w21- abc12346")
+
+//  v--- jump to this
+Foo.fmt
 
 Effect("I am an effect233")
   .flatMap(_ => Effect(_ + ", I am another cc effect or 11? hmh"))
